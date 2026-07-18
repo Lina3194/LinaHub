77cd29b67cddb9db5e6256eca2af5fb7c4e2e1d9
Lina3194 <ttv4mgf6dc@privaterelay.appleid.com>
2026-07-18 12:29:25 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE="linahub-v4-3-clean";
-const ASSETS=["./","./index.html","./style.css?v=43","./app.js?v=43","./manifest.webmanifest?v=43","./icons/icon-192.png?v=43","./icons/icon-512.png?v=43","./icons/apple-touch-icon.png?v=43"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{
- if(e.request.mode==="navigate"){e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match("./index.html")));return}
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x})))
-});

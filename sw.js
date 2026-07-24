3636aeb95ca0eac554a42ccf3c700096a787f833
Lina3194 <ttv4mgf6dc@privaterelay.appleid.com>
2026-07-24 22:13:46 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,11 +2,11 @@ const BUILD_VERSION = 'linahub-v17.0';
 /* LinaHub 17.0 */
 const CACHE="linahub-v17-1";
 const ASSETS=[
-"./","./index.html","./app.js?v=1711","./manifest.webmanifest?v=1711",
-"./core/pokemon-seed.js?v=1711","./core/data.js?v=1711","./core/router.js?v=1711","./core/cloud.js?v=1711",
-"./pages/home.js?v=1711","./pages/today.js?v=1711","./pages/todo.js?v=1711","./pages/shopping.js?v=1711","./pages/hobbies.js?v=1711","./pages/books.js?v=1711","./pages/journal.js?v=1711","./pages/plants.js?v=1711","./pages/pokemon.js?v=1711","./pages/house.js?v=1711","./pages/medication.js?v=1711","./pages/health.js?v=1711","./pages/simple.js?v=1711","./pages/aquariums.js?v=1711","./pages/period.js?v=1711","./pages/treasures.js?v=1711","./pages/budget.js?v=1711",
-"./styles/base.css?v=1711","./styles/home.css?v=1711","./styles/journal.css?v=1711","./styles/plants.css?v=1711","./styles/modules.css?v=1711","./styles/period.css?v=1711","./styles/treasures.css?v=1711",
-"./icons/icon-192.png?v=1711","./icons/icon-512.png?v=1711","./icons/apple-touch-icon.png?v=1711"
+"./","./index.html","./app.js?v=1713","./manifest.webmanifest?v=1713",
+"./core/pokemon-seed.js?v=1713","./core/data.js?v=1713","./core/router.js?v=1713","./core/cloud.js?v=1713",
+"./pages/home.js?v=1713","./pages/today.js?v=1713","./pages/todo.js?v=1713","./pages/shopping.js?v=1713","./pages/hobbies.js?v=1713","./pages/books.js?v=1713","./pages/journal.js?v=1713","./pages/plants.js?v=1713","./pages/pokemon.js?v=1713","./pages/house.js?v=1713","./pages/medication.js?v=1713","./pages/health.js?v=1713","./pages/simple.js?v=1713","./pages/aquariums.js?v=1713","./pages/period.js?v=1713","./pages/treasures.js?v=1713","./pages/budget.js?v=1713",
+"./styles/base.css?v=1713","./styles/home.css?v=1713","./styles/journal.css?v=1713","./styles/plants.css?v=1713","./styles/modules.css?v=1713","./styles/period.css?v=1713","./styles/treasures.css?v=1713",
+"./icons/icon-192.png?v=1713","./icons/icon-512.png?v=1713","./icons/apple-touch-icon.png?v=1713"
 ];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));

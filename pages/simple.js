ebf56bcaa139405e471de2f1d446797aaf140ff3
Lina3194 <ttv4mgf6dc@privaterelay.appleid.com>
2026-08-02 14:08:19 +0100

    Add files via upload

--- pages/simple.js
+++ pages/simple.js
@@ -109,7 +109,7 @@ function SettingsPage(){
       <button class="primary" id="exportData">Export backup</button>
       <label class="secondary" style="display:block;margin-top:10px">Import backup<input id="importData" type="file" accept="application/json" hidden></label>
     </section>
-  <p class="app-version">Version ${esc(window.LINAHUB_BUILD||"17.9.36")}<br><br>1 Aug 2026</p>`,"settings");
+  <p class="app-version">Version ${esc(window.LINAHUB_BUILD||"17.9.37")}<br><br>1 Aug 2026</p>`,"settings");
 }
 
 function bindSimple(){

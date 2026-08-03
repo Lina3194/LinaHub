8e174254f280ee6882f1d6b7d5e0d5d2fbc7e24b
Lina3194 <ttv4mgf6dc@privaterelay.appleid.com>
2026-08-03 12:02:37 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -537,7 +537,7 @@ if("serviceWorker" in navigator){navigator.serviceWorker.addEventListener("messa
 if("serviceWorker" in navigator){
   window.addEventListener("load",async()=>{
     try{
-      const registration=await navigator.serviceWorker.register("./sw.js?v=1832",{updateViaCache:"none"});
+      const registration=await navigator.serviceWorker.register("./sw.js?v=1833",{updateViaCache:"none"});
       await registration.update();
       let refreshed=false;
       navigator.serviceWorker.addEventListener("controllerchange",()=>{
